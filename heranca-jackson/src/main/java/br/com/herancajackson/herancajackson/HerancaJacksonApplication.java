b0361788cf4af28397513e8f226d83fcadf7220b
Fabio <fabioaugusto801@gmail.com>
2020-03-04 16:05:35 -0300

    Iniciando o desafio.

---
+++ heranca-jackson/src/main/java/br/com/herancajackson/herancajackson/HerancaJacksonApplication.java
@@ -0,0 +1,13 @@
+package br.com.herancajackson.herancajackson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerancaJacksonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerancaJacksonApplication.class, args);
+	}
+
+}
---
+++ heranca-jackson/src/test/java/br/com/herancajackson/herancajackson/HerancaJacksonApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.herancajackson.herancajackson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HerancaJacksonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
